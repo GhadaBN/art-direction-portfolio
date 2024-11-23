a763d0e6b8c3b7d046eced23d2779092ff4020ea
Ghada Bennasr <gh.bennasr@gmail.com>
2024-11-23 22:16:55 +0100

    project layout

---
+++ src/components/DemoVideo/DemoVideo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+const DemoVideo = ({ demoVideo }) => {
+  return (
+    <div className="cover-project-container">
+      {demoVideo?.map((video, index) => (
+        <div key={index} className="cover-item">
+          <ReactPlayer
+            url={video}
+            playing
+            loop
+            muted
+            controls={false}
+            width="100%"
+            height="100%"
+            className="project-video"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DemoVideo;
